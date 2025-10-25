c7206438c1932344f1197b878469e318462fd8c9
Ralph Plawetzki <ralph@purejava.org>
2025-10-25 19:01:18 +0200

    Fix module-info.java to solve DBusCallExceptions

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -7,4 +7,7 @@ module org.purejava.secret {
     exports org.purejava.secret.api.handlers;
     exports org.purejava.secret.api.errors;
     exports org.purejava.secret.api;
+    exports org.purejava.secret.interfaces to org.freedesktop.dbus;
+
+    opens org.purejava.secret.api to org.freedesktop.dbus;
 }
