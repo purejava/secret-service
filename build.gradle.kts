2c4d6dabe9d7c56a47225c084620f094dc97c7ee
Ralph Plawetzki <ralph@purejava.org>
2026-08-27 08:52:39 +0200

    Increase artifact version

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 }
 
 group = "org.purejava"
-version = "1.3.1"
+version = "1.3.2-SNAPSHOT"
 description = "A Java library for managing secrets on Linux using the secret service DBus interface"
 
 val releaseGradlePluginToken: String = System.getenv("RELEASE_GRADLE_PLUGIN_TOKEN") ?: ""
